f5a85f5dd72f9eb92bc3cc19a60b522f84c3b7f6
Kevin Baker <eire32kevin@gmail.com>
2015-02-03 22:35:43 +0000

    Remove strict ssh host check for index.html deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 # Deploy the index.html to my nginx server on EC2 by simply SCP'ing the file over
+echo -e "Host e32designs.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 chmod 600 .travis/deploy_key.pem
 ssh-add .travis/deploy_key.pem
 
